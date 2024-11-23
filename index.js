7abc0c410ef1f70d74052cc2af42417e6596ee52
0to16 <saksham.ahuja@0to16.in>
2024-11-23 12:18:57 +0530

    Added chrome API for saving the current tab

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-// chrome://extensions/just checking
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
 
 // Fetching leads from local storage
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
+    render(myLeads)
 }
-render(myLeads)
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
 
 function render(leads) {
     let listItems = ""
